7d6b0c5d9e14ff231172280773b4d934daa7523a
Ebuka Nwabueze <ebukanwabueze@gmail.com>
2022-03-24 01:09:56 +0100

    added intial pages to the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div >
-      React App
-    </div>
+    <>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
