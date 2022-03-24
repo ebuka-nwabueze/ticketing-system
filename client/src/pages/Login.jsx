f21f52104525dccffb451ff10af1d7cb20a9a20c
Ebuka Nwabueze <ebukanwabueze@gmail.com>
2022-03-24 02:38:53 +0100

    Login, Register and Home page edited

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,70 @@
+import { useState } from "react";
+import { FaSignInAlt } from "react-icons/fa";
+import { toast } from "react-toastify";
+
+
 function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+  };
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
   return (
-    <div>
-      Login
-    </div>
-  )
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Register
+        </h1>
+        <p>Login to get support</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+
+          <div className="form-group">
+            <input
+              type="text"
+              name="email"
+              id="email"
+              value={email}
+              placeholder="Enter your email"
+              onChange={onChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              placeholder="Enter password"
+              required
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <button className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
 
-export default Login
+export default Login;
